1dd0cb6d793770b0180cb5da0a6c0ff770715c60
Rafał <rafaczk@gmail.com>
2023-08-15 13:17:32 +0200

    Problem 66. Plus One solution
    Runtime 122 ms Beats 93.18%
    Memory 42.3 MB Beats 91.54%

---
+++ LC.test/PlusOne.cs
@@ -0,0 +1,68 @@
+﻿namespace LC.test
+{
+	public class PlusOne
+	{
+		[Fact]
+		public void TestExample1()
+		{
+			int[] input = { 1, 2, 3 };
+			int[] expected = { 1, 2, 4 };
+			int[] result = LC.PlusOne.PlusOn(input);
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void TestExample2()
+		{
+			int[] input = { 4, 3, 2, 1 };
+			int[] expected = { 4, 3, 2, 2 };
+			int[] result = LC.PlusOne.PlusOn(input);
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void TestExample3()
+		{
+			int[] input = { 9 };
+			int[] expected = { 1, 0 };
+			int[] result = LC.PlusOne.PlusOn(input);
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void TestExample4()
+		{
+			int[] input = { 9, 9 };
+			int[] expected = { 1, 0, 0 };
+			int[] result = LC.PlusOne.PlusOn(input);
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void TestExample5()
+		{
+			int[] input = { 9, 9, 9 };
+			int[] expected = { 1, 0, 0, 0 };
+			int[] result = LC.PlusOne.PlusOn(input);
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void TestExample6()
+		{
+			int[] input = { 8, 9, 9, 9 };
+			int[] expected = { 9, 0, 0, 0 };
+			int[] result = LC.PlusOne.PlusOn(input);
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void TestExample7()
+		{
+			int[] input = { 9, 8, 9 };
+			int[] expected = { 9, 9, 0 };
+			int[] result = LC.PlusOne.PlusOn(input);
+			Assert.Equal(expected, result);
+		}
+	}
+}
---
+++ LC/PlusOne.cs
@@ -0,0 +1,51 @@
+﻿namespace LC
+{
+	//66. Plus One
+	public class PlusOne
+	{
+		public static int[] PlusOn(int[] digits)
+		{
+			if (digits[digits.Length - 1] != 9)
+			{
+				digits[digits.Length - 1] += 1;
+				return digits;
+			}
+
+			bool resize = default;
+
+			for (int i = digits.Length - 1; i > -1; i--)
+			{
+				if (digits[i] != 9)
+				{
+					if (digits[i] == 9 && i == 0)
+					{
+						resize = true;
+					}
+					digits[i] += 1;
+					break;
+				}
+
+				if (i != 0 && digits[i] == 9)
+				{
+					digits[i] = 0;
+				}
+
+				if (i == 0)
+				{
+					if (digits[i] == 9)
+					{
+						digits[i] = 1;
+						resize = true;
+					}
+				}
+			}
+
+			if (resize)
+			{
+				Array.Resize(ref digits, digits.Length + 1);
+			}
+
+			return digits;
+		}
+	}
+}
