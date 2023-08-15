1ff5d7d324c2a11e56c8de56fee5b9f19993eaa4
Rafał <rafaczk@gmail.com>
2023-08-15 14:23:47 +0200

    Problem 172. Factorial Trailing Zeroes solution
    Runtime 16ms Beats 97.96% of users with C#
    Memory 26.58mb Beats 66.67% of users with C#

---
+++ LC.test/FactorialTrailingZeroes.cs
@@ -0,0 +1,104 @@
+﻿namespace LC.test
+{
+	public class FactorialTrailingZeroes
+	{
+		[Fact]
+		public void TestTrailingZeroes_Case1()
+		{
+			// Arrange
+			int input = 3;
+			int expected = 0;
+
+			// Act
+			int result = LC.FactorialTrailingZeroes.TrailingZeroes(input); //6
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void TestTrailingZeroes_Case2()
+		{
+			// Arrange
+			int input = 5;
+			int expected = 1;
+
+			// Act
+			int result = LC.FactorialTrailingZeroes.TrailingZeroes(input); //120
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void TestTrailingZeroes_Case3()
+		{
+			// Arrange
+			int input = 0;
+			int expected = 0;
+
+			// Act
+			int result = LC.FactorialTrailingZeroes.TrailingZeroes(input); // 0
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void TestTrailingZeroes_Case4()
+		{
+			// Arrange
+			int input = 7;
+			int expected = 1;
+
+			// Act
+			int result = LC.FactorialTrailingZeroes.TrailingZeroes(input); // 0
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void TestTrailingZeroes_Case5()
+		{
+			// Arrange
+			int input = 13;
+			int expected = 2;
+
+			// Act
+			int result = LC.FactorialTrailingZeroes.TrailingZeroes(input); // 0
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void TestTrailingZeroes_Case6()
+		{
+			// Arrange
+			int input = 30;
+			int expected = 7;
+
+			// Act
+			int result = LC.FactorialTrailingZeroes.TrailingZeroes(input); // 0
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+
+		[Fact]
+		public void TestTrailingZeroes_Case7()
+		{
+			// Arrange
+			int input = 50;
+			int expected = 12;
+
+			// Act
+			int result = LC.FactorialTrailingZeroes.TrailingZeroes(input); // 0
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+	}
+}
