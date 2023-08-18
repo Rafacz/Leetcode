d4e40d425ca439c6170770c7967653733bc703de
Rafał <rafaczk@gmail.com>
2023-08-18 19:30:19 +0200

    242. Valid Anagram solution
    Runtime Beats 95.32% of users with C#
    Memory Beats 85.01% of users with C#

---
+++ LC/ValidAnagram.cs
@@ -0,0 +1,29 @@
+﻿namespace LC
+{
+	//242. Valid Anagram
+	public class ValidAnagram
+	{
+		public static bool IsAnagram(string s, string t)
+		{
+			if (s.Length != t.Length) return false;
+
+			int[] chars = new int[26];
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				chars[s[i] - 'a'] += 1;
+				chars[t[i] - 'a'] -= 1;
+			}
+
+			foreach (int c in chars)
+			{
+				if (c != 0)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
