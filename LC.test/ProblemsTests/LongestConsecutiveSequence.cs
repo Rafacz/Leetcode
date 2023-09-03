365bc250991b75d8ce657766b01843c1252e8a3f
Rafał <rafaczk@gmail.com>
2023-09-03 18:47:19 +0200

    Longest Consecutive Sequence not optiomal

---
+++ LC.test/ProblemsTests/LongestConsecutiveSequence.cs
@@ -0,0 +1,38 @@
+﻿namespace LC.test.ProblemsTests
+{
+	public class LongestConsecutiveSequence
+	{
+		[Fact]
+		public void TestExample1()
+		{
+			int[] nums = { 100, 4, 200, 1, 3, 2 };
+			int expected = 4;
+
+			int result = Problems.LongestConsecutiveSequence.LongestConsecutive(nums);
+
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void TestExample2()
+		{
+			int[] nums = { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
+			int expected = 9;
+
+			int result = Problems.LongestConsecutiveSequence.LongestConsecutive(nums);
+
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void TestExample3()
+		{
+			int[] nums = { 9, 1, -3, 2, 4, 8, 3, -1, 6, -2, -4, 7 };
+			int expected = 4;
+
+			int result = Problems.LongestConsecutiveSequence.LongestConsecutive(nums);
+
+			Assert.Equal(expected, result);
+		}
+	}
+}
---
+++ LC/Problems/LongestConsecutiveSequence.cs
@@ -0,0 +1,40 @@
+﻿namespace LC.Problems
+{
+	public static class LongestConsecutiveSequence
+	{
+		public static int LongestConsecutive(int[] nums)
+		{
+			if (nums == null || nums.Length == 0) return 0;
+
+			var hs = new HashSet<int>(nums);
+
+			int num = hs.Min();
+			int highest = hs.Max();
+			int max = 1;
+
+			int result = default;
+
+			while (num <= highest)
+			{
+				if (hs.Contains(num = num + 1))
+				{
+					max++;
+				}
+				else
+				{
+					if (max > result)
+					{
+						result = max;
+						if (result > nums.Length / 2)
+						{
+							return result;
+						}
+					}
+					max = 0;
+				}
+			}
+
+			return result;
+		}
+	}
+}
