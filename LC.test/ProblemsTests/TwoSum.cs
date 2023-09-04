3d05e94b2b71b8a7836c0718b9dcf35fa6e4d8fd
Rafał <rafaczk@gmail.com>
2023-09-04 18:12:42 +0200

    TwoSum solution

---
+++ LC.test/ProblemsTests/TwoSum.cs
@@ -0,0 +1,42 @@
+﻿namespace LC.test.ProblemsTests
+{
+	public class TwoSum
+	{
+
+		[Fact]
+		public void Example1()
+		{
+			// Arrange
+			int[] nums = { 2, 7, 11, 15 };
+			int target = 9;
+			int[] result = Problems.TwoSum.TwoSums(nums, target);
+
+			// Act & Assert
+			Assert.Equal(new int[] { 0, 1 }, result);
+		}
+
+		[Fact]
+		public void Example2()
+		{
+			// Arrange
+			int[] nums = { 3, 2, 4 };
+			int target = 6;
+			int[] result = Problems.TwoSum.TwoSums(nums, target);
+
+			// Act & Assert
+			Assert.Equal(new int[] { 1, 2 }, result);
+		}
+
+		[Fact]
+		public void Example3()
+		{
+			// Arrange
+			int[] nums = { 3, 3 };
+			int target = 6;
+			int[] result = Problems.TwoSum.TwoSums(nums, target);
+
+			// Act & Assert
+			Assert.Equal(new int[] { 0, 1 }, result);
+		}
+	}
+}
