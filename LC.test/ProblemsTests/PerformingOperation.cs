062341caa05ba9df7a02de45642dedbd6c6bdf67
Rafał <rafaczk@gmail.com>
2023-09-04 17:48:04 +0200

    Value of Variable After Performing Operations Solution

---
+++ LC.test/ProblemsTests/PerformingOperation.cs
@@ -0,0 +1,38 @@
+﻿namespace LC.test.ProblemsTests
+{
+	public class PerformingOperation
+	{
+		[Fact]
+		public void Example1()
+		{
+			// Arrange
+			string[] operations = { "--X", "X++", "X++" };
+			int result = Problems.PerformingOperations.FinalValueAfterOperations(operations);
+
+			// Act & Assert
+			Assert.Equal(1, result);
+		}
+
+		[Fact]
+		public void Example2()
+		{
+			// Arrange
+			string[] operations = { "++X", "++X", "X++" };
+			int result = Problems.PerformingOperations.FinalValueAfterOperations(operations);
+
+			// Act & Assert
+			Assert.Equal(3, result);
+		}
+
+		[Fact]
+		public void Example3()
+		{
+			// Arrange
+			string[] operations = { "X++", "++X", "--X", "X--" };
+			int result = Problems.PerformingOperations.FinalValueAfterOperations(operations);
+
+			// Act & Assert
+			Assert.Equal(0, result);
+		}
+	}
+}
---
+++ LC/Problems/PerformingOperations.cs
@@ -0,0 +1,25 @@
+﻿namespace LC.Problems
+{
+	//2011. Final Value of Variable After Performing Operations
+	public static class PerformingOperations
+	{
+		public static int FinalValueAfterOperations(string[] operations)
+		{
+			int result = default;
+
+			foreach (var operation in operations)
+			{
+				if (operation[1] == '-')
+				{
+					result--;
+				}
+				else
+				{
+					result++;
+				}
+			}
+
+			return result;
+		}
+	}
+}
