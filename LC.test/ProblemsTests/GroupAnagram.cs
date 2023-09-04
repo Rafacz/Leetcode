984cc5a374b8c674978b768faedf67f9914e0fa4
Rafał <rafaczk@gmail.com>
2023-09-04 11:30:38 +0200

    Group Anagrams Solution

---
+++ LC.test/ProblemsTests/GroupAnagram.cs
@@ -0,0 +1,47 @@
+﻿namespace LC.test.ProblemsTests
+{
+	public class GroupAnagram
+	{
+		[Fact]
+		public void GroupAnagrams_Example1_ReturnsCorrectResult()
+		{
+			// Arrange
+			string[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+			string[][] expectedOutput = { new string[] { "bat" }, new string[] { "nat", "tan" }, new string[] { "ate", "eat", "tea" } };
+
+			// Act
+			var result = Problems.GroupAnagram.GroupAnagrams(strs);
+
+			// Assert
+			Assert.Equal(expectedOutput, result);
+		}
+
+		[Fact]
+		public void GroupAnagrams_Example2_ReturnsCorrectResult()
+		{
+			// Arrange
+			string[] strs = { "" };
+			string[][] expectedOutput = { new string[] { "" } };
+
+			// Act
+			var result = Problems.GroupAnagram.GroupAnagrams(strs);
+
+			// Assert
+			Assert.Equal(expectedOutput, result);
+		}
+
+		[Fact]
+		public void GroupAnagrams_Example3_ReturnsCorrectResult()
+		{
+			// Arrange
+			string[] strs = { "a" };
+			string[][] expectedOutput = { new string[] { "a" } };
+
+			// Act
+			var result = Problems.GroupAnagram.GroupAnagrams(strs);
+
+			// Assert
+			Assert.Equal(expectedOutput, result);
+		}
+	}
+}
---
+++ LC/Problems/GroupAnagrams.cs
@@ -0,0 +1,26 @@
+﻿namespace LC.Problems
+{
+	//49. Group Anagrams
+	public static class GroupAnagram
+	{
+		public static IList<IList<string>> GroupAnagrams(string[] strs)
+		{
+			var dict = new Dictionary<string, List<string>>();
+
+			foreach (var str in strs)
+			{
+				var charArr = str.ToCharArray();
+				Array.Sort(charArr);
+				var sortedStr = new string(charArr);
+
+				if (!dict.ContainsKey(sortedStr))
+				{
+					dict[sortedStr] = new List<string>();
+				}
+
+				dict[sortedStr].Add(str);
+			}
+			return new List<IList<string>>(dict.Values);
+		}
+	}
+}
