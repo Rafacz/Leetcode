33e1f88a07d13ca941ec8a8cb2a6310e1c86cbde
Rafał <rafaczk@gmail.com>
2023-09-05 11:54:17 +0200

    Search Maximum Depth of Binary Tree solution

---
+++ LC.test/BinaryTree/MaximumDepthBinaryTree.cs
@@ -0,0 +1,38 @@
+﻿using LC.BinaryTree;
+
+namespace LC.test.BinaryTree
+{
+	public class MaximumDepthBinaryTree
+	{
+		[Fact]
+		public void TestExample1()
+		{
+			// Arrange
+			TreeNode root = new TreeNode(3);
+			root.left = new TreeNode(9);
+			root.right = new TreeNode(20);
+			root.right.left = new TreeNode(15);
+			root.right.right = new TreeNode(7);
+
+			// Act
+			int result = LC.BinaryTree.MaximumDepthBinaryTree.MaxDepth(root);
+
+			// Assert
+			Assert.Equal(3, result);
+		}
+
+		[Fact]
+		public void TestExample2()
+		{
+			// Arrange
+			TreeNode root = new TreeNode(1);
+			root.right = new TreeNode(2);
+
+			// Act
+			int result = LC.BinaryTree.MaximumDepthBinaryTree.MaxDepth(root);
+
+			// Assert
+			Assert.Equal(2, result);
+		}
+	}
+}
---
+++ LC/BinaryTree/MaximumDepthBinaryTree.cs
@@ -0,0 +1,40 @@
+﻿namespace LC.BinaryTree
+{
+	public static class MaximumDepthBinaryTree
+	{
+		public static int MaxDepth(TreeNode root)
+		{
+			if (root == null) { return 0; }
+			int depth = default;
+			int max = default;
+
+			var stack = new Stack<(TreeNode, int)>();
+			stack.Push((root, depth));
+
+			while (stack.Count > 0)
+			{
+				var node = stack.Pop();
+				depth = node.Item2 + 1;
+				if (node.Item1.right != null) stack.Push((node.Item1.right, depth));
+				if (node.Item1.left != null) stack.Push((node.Item1.left, depth));
+
+				if (depth > max) max = depth;
+			}
+
+			return max;
+		}
+	}
+
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+		{
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
