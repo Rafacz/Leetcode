497441d1953914753f8420f416f249a42a840fa5
Rafał <rafaczk@gmail.com>
2023-09-06 23:00:30 +0200

    Count Tree Nodes in Binary Tree solution

---
+++ LC.test/BinaryTree/CountCompleteTreeNodes.cs
@@ -0,0 +1,53 @@
+﻿using LC.BinaryTree;
+
+namespace LC.test.BinaryTree
+{
+	public class CountCompleteTreeNodes
+	{
+		[Fact]
+		public void Example1()
+		{
+			// Arrange
+			TreeNode root = new TreeNode(
+				1,
+				new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+				new TreeNode(3, new TreeNode(6))
+			);
+			int expected = 6;
+
+			// Act
+			int result = LC.BinaryTree.CountCompleteTreeNodes.CountNodes(root);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Example2()
+		{
+			// Arrange
+			TreeNode root = null;
+			int expected = 0;
+
+			// Act
+			int result = LC.BinaryTree.CountCompleteTreeNodes.CountNodes(root);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Example3()
+		{
+			// Arrange
+			TreeNode root = new TreeNode(1);
+			int expected = 1;
+
+			// Act
+			int result = LC.BinaryTree.CountCompleteTreeNodes.CountNodes(root);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+	}
+}
---
+++ LC/BinaryTree/CountCompleteTreeNodes.cs
@@ -0,0 +1,25 @@
+﻿namespace LC.BinaryTree
+{
+	//222. Count Complete Tree Nodes
+	public static class CountCompleteTreeNodes
+	{
+		public static int CountNodes(TreeNode root)
+		{
+			if (root == null) { return 0; }
+			int count = default;
+
+			var stack = new Stack<TreeNode>();
+			stack.Push(root);
+
+			while (stack.Count > 0)
+			{
+				var node = stack.Pop();
+				count++;
+				if (node.right != null) stack.Push((node.right));
+				if (node.left != null) stack.Push((node.left));
+			}
+
+			return count;
+		}
+	}
+}
