ca02435d63d7529dc90280cbe91fc5a5d96d9b27
Rafał <rafaczk@gmail.com>
2023-09-07 19:54:40 +0200

    Average of Levels in Binary Tree solution

---
+++ LC.test/BinaryTree/AverageLevels.cs
@@ -0,0 +1,42 @@
+﻿using LC.BinaryTree;
+
+namespace LC.test.BinaryTree
+{
+	public class AverageLevels
+	{
+
+		[Fact]
+		public void Test1()
+		{
+			// Arrange
+			TreeNode root = new TreeNode(3);
+			root.left = new TreeNode(9);
+			root.right = new TreeNode(20);
+			root.right.left = new TreeNode(15);
+			root.right.right = new TreeNode(7);
+
+			// Act
+			var result = LC.BinaryTree.AverageLevels.AverageOfLevels(root);
+
+			// Assert
+			Assert.Equal(new double[] { 3.0, 14.5, 11.0 }, result);
+		}
+
+		[Fact]
+		public void Test2()
+		{
+			// Arrange
+			TreeNode root = new TreeNode(3);
+			root.left = new TreeNode(9);
+			root.right = new TreeNode(20);
+			root.right.left = new TreeNode(15);
+			root.right.right = new TreeNode(7);
+
+			// Act
+			var result = LC.BinaryTree.AverageLevels.AverageOfLevels(root);
+
+			// Assert
+			Assert.Equal(new double[] { 3.0, 14.5, 11.0 }, result);
+		}
+	}
+}
---
+++ LC/BinaryTree/AverageLevels.cs
@@ -0,0 +1,39 @@
+﻿namespace LC.BinaryTree
+{
+	//637. Average of Levels in Binary Tree
+	public static class AverageLevels
+	{
+		public static IList<double> AverageOfLevels(TreeNode root)
+		{
+			var result = new List<double>();
+
+			if (root == null)
+				return result;
+
+			var q = new Queue<TreeNode>();
+
+			q.Enqueue(root);
+
+			while (q.Count > 0)
+			{
+				int count = q.Count;
+				double sum = 0;
+
+				for (int i = 0; i < count; i++)
+				{
+					TreeNode node = q.Dequeue();
+					sum += node.val;
+
+					if (node.left != null)
+						q.Enqueue(node.left);
+					if (node.right != null)
+						q.Enqueue(node.right);
+				}
+
+				result.Add(sum / count);
+			}
+
+			return result;
+		}
+	}
+}
