2ceb7e8d767c37abe1f6358fb5cd668a83780544
Rafał <rafaczk@gmail.com>
2023-09-18 17:07:08 +0200

    1337. The K Weakest Rows in a Matrix - solution

---
+++ LC.test/Matrix/WeakestRows.cs
@@ -0,0 +1,77 @@
+﻿namespace LC.test.Matrix
+{
+	public class WeakestRows
+	{
+		[Fact]
+		public void Example1()
+		{
+			int[][] mat = new int[][] {
+			new int[] { 1, 1, 0, 0, 0 },
+			new int[] { 1, 1, 1, 1, 0 },
+			new int[] { 1, 0, 0, 0, 0 },
+			new int[] { 1, 1, 0, 0, 0 },
+			new int[] { 1, 1, 1, 1, 1 }
+		};
+			int k = 3;
+			int[] expected = new int[] { 2, 0, 3 };
+
+			int[] result = LC.Matrix.WeakestRows.KWeakestRows(mat, k);
+
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Example2()
+		{
+			int[][] mat = new int[][] {
+			new int[] { 1, 0, 0, 0 },
+			new int[] { 1, 1, 1, 1 },
+			new int[] { 1, 0, 0, 0 },
+			new int[] { 1, 0, 0, 0 }
+		};
+			int k = 2;
+			int[] expected = new int[] { 0, 2 };
+
+			int[] result = LC.Matrix.WeakestRows.KWeakestRows(mat, k);
+
+			Assert.Equal(expected, result);
+		}
+
+
+		[Fact]
+		public void Example13()
+		{
+			int[][] mat = new int[][] {
+			new int[] { 1, 0 },
+			new int[] { 1, 0 },
+			new int[] { 1, 0 },
+			new int[] { 1, 1 }
+		};
+			int k = 4;
+			int[] expected = new int[] { 0, 1, 2, 3 };
+
+			int[] result = LC.Matrix.WeakestRows.KWeakestRows(mat, k);
+
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Example4()
+		{
+			int[][] mat = new int[][] {
+			new int[] { 1, 1, 0 },
+			new int[] { 1, 0, 0 },
+			new int[] { 1, 0, 0 },
+			new int[] { 1, 1, 1 },
+			new int[] { 1, 1, 0 },
+			new int[] { 0, 0, 0 }
+		};
+			int k = 4;
+			int[] expected = new int[] { 5, 1, 2, 0 };
+
+			int[] result = LC.Matrix.WeakestRows.KWeakestRows(mat, k);
+
+			Assert.Equal(expected, result);
+		}
+	}
+}
---
+++ LC/Matrix/WeakestRows.cs
@@ -0,0 +1,51 @@
+﻿namespace LC.Matrix
+{
+	//1337. The K Weakest Rows in a Matrix
+	public static class WeakestRows
+	{
+		public static int[] KWeakestRows(int[][] mat, int k)
+		{
+			var result = new HashSet<int>(k);
+			int rows = mat.GetLength(0);
+			int cols = mat[0].GetLength(0);
+
+			int p = 0;
+
+			for (int y = 0; y < cols; y++)
+			{
+				for (int x = 0; x < rows; x++)
+				{
+					if (mat[x][y] == 0 && !result.Contains(x))
+					{
+						result.Add(x);
+						p++;
+
+						if (p == k)
+						{
+							return result.ToArray();
+						}
+					}
+				}
+			}
+
+			if (p == 0)
+			{
+				return new int[] { 0 };
+			}
+
+			if (result.Count != k)
+			{
+				for (int x = 0; x < rows; x++)
+				{
+					result.Add(x);
+					if (result.Count == k)
+					{
+						return result.ToArray();
+					}
+				}
+			}
+
+			return result.ToArray();
+		}
+	}
+}
