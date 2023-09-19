5d76fa4b6ff939e6e9e8c6884780599cdd4bee3a
Rafał <rafaczk@gmail.com>
2023-09-19 14:27:02 +0200

    Text Justification - solution

---
+++ LC.test/String/TextJustification.cs
@@ -0,0 +1,93 @@
+﻿namespace LC.test.String
+{
+	public class TextJustification
+	{
+		[Fact]
+		public void Example1()
+		{
+			string[] words = { "This", "is", "an", "example", "of", "text", "justification." };
+			int maxWidth = 16;
+			List<string> expectedOutput = new List<string>
+		{
+			"This    is    an",
+			"example  of text",
+			"justification.  "
+		};
+
+			IList<string> result = LC.String.TextJustification.FullJustify(words, maxWidth);
+			Assert.Equal(expectedOutput, result);
+		}
+
+		[Fact]
+		public void Example2()
+		{
+			string[] words = { "What", "must", "be", "acknowledgment", "shall", "be" };
+			int maxWidth = 16;
+			List<string> expectedOutput = new List<string>
+			{
+				"What   must   be",
+				"acknowledgment  ",
+				"shall be        "
+			};
+
+			IList<string> result = LC.String.TextJustification.FullJustify(words, maxWidth);
+			Assert.Equal(expectedOutput, result);
+		}
+
+		[Fact]
+		public void Example3()
+		{
+			string[] words = { "Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do" };
+			int maxWidth = 20;
+			List<string> expectedOutput = new List<string>
+			{
+				"Science  is  what we",
+				"understand      well",
+				"enough to explain to",
+				"a  computer.  Art is",
+				"everything  else  we",
+				"do                  "
+			};
+
+			IList<string> result = LC.String.TextJustification.FullJustify(words, maxWidth);
+			Assert.Equal(expectedOutput, result);
+		}
+
+		[Fact]
+		public void Example4()
+		{
+			string[] words = { "The", "important", "thing", "is", "not", "to", "stop", "questioning.", "Curiosity", "has", "its", "own", "reason", "for", "existing." };
+			int maxWidth = 17;
+			List<string> expectedOutput = new List<string>
+			{
+				"The     important",
+				"thing  is  not to",
+				"stop questioning.",
+				"Curiosity has its",
+				"own   reason  for",
+				"existing.        "
+			};
+
+			IList<string> result = LC.String.TextJustification.FullJustify(words, maxWidth);
+			Assert.Equal(expectedOutput, result);
+		}
+
+		[Fact]
+		public void Example5()
+		{
+			string[] words = { "ask", "not", "what", "your", "country", "can", "do", "for", "you", "ask", "what", "you", "can", "do", "for", "your", "country" };
+			int maxWidth = 16;
+			List<string> expectedOutput = new List<string>
+			{
+				"ask   not   what",
+				"your country can",
+				"do  for  you ask",
+				"what  you can do",
+				"for your country"
+			};
+
+			IList<string> result = LC.String.TextJustification.FullJustify(words, maxWidth);
+			Assert.Equal(expectedOutput, result);
+		}
+	}
+}
---
+++ LC/String/TextJustification.cs
@@ -0,0 +1,93 @@
+﻿using System.Text;
+
+namespace LC.String
+{
+	//68. Text Justification
+	public static class TextJustification
+	{
+		public static IList<string> FullJustify(string[] words, int maxWidth)
+		{
+			var result = new List<string>();
+			var sb = new StringBuilder(maxWidth);
+
+			int len = 0;
+			int b = -1;
+			int p = 0;
+
+			for (int x = 0; x < words.Length; x++)
+			{
+				len += words[x].Length;
+				b++;
+
+				if (len + b >= maxWidth || x == words.Length - 1)
+				{
+					if (len + b > maxWidth)
+					{
+						len = len - words[x].Length;
+						b--;
+						x--;
+					}
+
+					if (x == words.Length - 1)
+					{
+						for (int y = p; y < x + 1; y++)
+						{
+							if (y != words.Length - 1)
+							{
+								sb.Append(words[y] + ' ');
+							}
+							else
+							{
+								sb.Append(words[y] + new string(' ', maxWidth - len - b));
+								result.Add(sb.ToString());
+								return result;
+							}
+						}
+					}
+
+					for (int j = p; j < x + 1; j++)
+					{
+						if (j >= x)
+						{
+							sb.Append(words[j]);
+
+							if (sb.Length > maxWidth)
+							{
+								int i = sb.Length - 1;
+								while (sb.Length > maxWidth)
+								{
+									if (sb[i] == ' ')
+									{
+										sb.Remove(i, 1);
+										i -= 2;
+									}
+
+									i--;
+								}
+							}
+
+							if (sb.Length < maxWidth)
+							{
+								sb.Append(new string(' ', maxWidth - words[x].Length));
+							}
+
+							result.Add(sb.ToString());
+							sb.Clear();
+						}
+						else
+						{
+							sb.Append(words[j]);
+							sb.Append(new string(' ', (int)Math.Ceiling((double)(maxWidth - len) / b)));
+						}
+					}
+
+					len = 0;
+					b = -1;
+					p = x + 1;
+				}
+			}
+
+			return result;
+		}
+	}
+}
