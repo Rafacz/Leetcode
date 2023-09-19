1b5a7db241e6b7b328ee79977b0a14d8c630a905
Rafał <rafaczk@gmail.com>
2023-09-19 19:15:45 +0200

    Contains Duplicate - solution

---
+++ LC.test/Array/ContainsDuplicate.cs
@@ -0,0 +1,47 @@
+﻿namespace LC.test.Array
+{
+	public class ContainsDuplicate
+	{
+		[Fact]
+		public void Example1()
+		{
+			// Arrange
+			int[] nums = { 1, 2, 3, 1 };
+			bool expected = true;
+
+			// Act
+			bool result = Arrays.ContainsDuplicate.ContainsDuplicates(nums);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Example2()
+		{
+			// Arrange
+			int[] nums = { 1, 2, 3, 4 };
+			bool expected = false;
+
+			// Act
+			bool result = Arrays.ContainsDuplicate.ContainsDuplicates(nums);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Example3()
+		{
+			// Arrange
+			int[] nums = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+			bool expected = true;
+
+			// Act
+			bool result = Arrays.ContainsDuplicate.ContainsDuplicates(nums);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+	}
+}
---
+++ LC/Arrays/ContainsDuplicate.cs
@@ -0,0 +1,19 @@
+﻿namespace LC.Arrays
+{
+	public static class ContainsDuplicate
+	{
+		//217. Contains Duplicate
+		public static bool ContainsDuplicates(int[] nums)
+		{
+			var hs = new HashSet<int>(nums.Length);
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (hs.Contains(nums[i])) return true;
+				hs.Add(nums[i]);
+			}
+
+			return false;
+		}
+	}
+}
