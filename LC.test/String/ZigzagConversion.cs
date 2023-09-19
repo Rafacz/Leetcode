5348d696e0732f7c4689d551f8d5dd335a0a3c10
Rafał <rafaczk@gmail.com>
2023-09-19 15:34:00 +0200

    Zigzag Conversion - solution

---
+++ LC.test/String/ZigzagConversion.cs
@@ -0,0 +1,95 @@
+﻿namespace LC.test.String
+{
+	public class ZigzagConversion
+	{
+		[Fact]
+		public void Example1()
+		{
+			// Arrange
+			string s = "PAYPALISHIRING";
+			int numRows = 3;
+			string expected = "PAHNAPLSIIGYIR";
+
+			// Act
+			string result = LC.String.ZigzagConversion.Convert(s, numRows);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Example2()
+		{
+			// Arrange
+			string s = "PAYPALISHIRING";
+			int numRows = 4;
+			string expected = "PINALSIGYAHRPI";
+
+			// Act
+			string result = LC.String.ZigzagConversion.Convert(s, numRows);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Example3()
+		{
+			// Arrange
+			string s = "A";
+			int numRows = 1;
+			string expected = "A";
+
+			// Act
+			string result = LC.String.ZigzagConversion.Convert(s, numRows);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Example4()
+		{
+			// Arrange
+			string s = "AB";
+			int numRows = 1;
+			string expected = "AB";
+
+			// Act
+			string result = LC.String.ZigzagConversion.Convert(s, numRows);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Example5()
+		{
+			// Arrange
+			string s = "ABC";
+			int numRows = 2;
+			string expected = "ACB";
+
+			// Act
+			string result = LC.String.ZigzagConversion.Convert(s, numRows);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Example6()
+		{
+			// Arrange
+			string s = "ABC";
+			int numRows = 3;
+			string expected = "ABC";
+
+			// Act
+			string result = LC.String.ZigzagConversion.Convert(s, numRows);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+	}
+}
---
+++ LC/String/ZigzagConversion.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace LC.String
+{
+	//6. Zigzag Conversion
+	public class ZigzagConversion
+	{
+		public static string Convert(string s, int numRows)
+		{
+			if (numRows == 1 || s.Length < 3) return s;
+
+			int[] indexes = new int[s.Length];
+			char[] characters = new char[s.Length];
+			int index = 0;
+			bool add = true;
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				if (index == numRows - 1) add = false;
+				if (index == 0) add = true;
+
+				indexes[i] = index;
+				characters[i] = s[i];
+
+				if (add)
+					index++;
+				else
+					index--;
+			}
+
+			StringBuilder sb = new StringBuilder();
+
+			for (int i = 0; i < numRows; i++)
+			{
+				for (int j = 0; j < s.Length; j++)
+				{
+					if (indexes[j] == i)
+					{
+						sb.Append(characters[j]);
+					}
+				}
+			}
+
+			return sb.ToString();
+		}
+	}
+}
