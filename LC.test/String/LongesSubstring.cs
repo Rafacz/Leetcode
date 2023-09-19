277ad1e0b6e7366bba80b0a3c6d22416892dd97e
Rafał <rafaczk@gmail.com>
2023-09-19 14:27:42 +0200

    Longest Substring Without Repeating Characters - solution

---
+++ LC.test/String/LongesSubstring.cs
@@ -0,0 +1,89 @@
+﻿namespace LC.test.String
+{
+	public class LongesSubstring
+	{
+		[Fact]
+		public void Test1()
+		{
+			// Arrange
+			string s = "abcabcbb";
+			int expected = 3;
+
+			// Act
+			int result = LC.String.LongesSubstring.LengthOfLongestSubstring(s);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Test2()
+		{
+			// Arrange
+			string s = "bbbbb";
+			int expected = 1;
+
+			// Act
+			int result = LC.String.LongesSubstring.LengthOfLongestSubstring(s);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Test3()
+		{
+			// Arrange
+			string s = "pwwkew";
+			int expected = 3;
+
+			// Act
+			int result = LC.String.LongesSubstring.LengthOfLongestSubstring(s);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Test4()
+		{
+			// Arrange
+			string s = " ";
+			int expected = 1;
+
+			// Act
+			int result = LC.String.LongesSubstring.LengthOfLongestSubstring(s);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Test5()
+		{
+			// Arrange
+			string s = "aab";
+			int expected = 2;
+
+			// Act
+			int result = LC.String.LongesSubstring.LengthOfLongestSubstring(s);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Test6()
+		{
+			// Arrange
+			string s = "dvdf";
+			int expected = 3;
+
+			// Act
+			int result = LC.String.LongesSubstring.LengthOfLongestSubstring(s);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+	}
+}
---
+++ LC/String/LongesSubstring.cs
@@ -0,0 +1,34 @@
+﻿namespace LC.String
+{
+	//3. Longest Substring Without Repeating Characters
+	public static class LongesSubstring
+	{
+		public static int LengthOfLongestSubstring(string s)
+		{
+			if (s is null) return 0;
+
+			var distinct = new HashSet<char>();
+
+			int max = 0;
+			int p = 0;
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				if (distinct.Contains(s[i]))
+				{
+					if (max < distinct.Count)
+					{
+						max = distinct.Count;
+					}
+
+					distinct.Clear();
+					i = p++;
+				}
+
+				distinct.Add(s[i]);
+			}
+
+			return Math.Max(max, distinct.Count);
+		}
+	}
+}
